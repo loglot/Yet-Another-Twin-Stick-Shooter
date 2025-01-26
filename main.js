149d042bf8934d0e0fd627d6db635cabd9396208
loglot <prattlog3@gmail.com>
2025-01-26 12:08:09 -0800

    added basic dash and enemy system

--- main.js
+++ main.js
@@ -15,7 +15,19 @@ document.addEventListener("mousemove", function(e) {
       y: (e.pageY - (window.innerHeight-ctx.canvas.height)/2)/display.scale
     };
 })
-// resize()
+document.addEventListener("mousedown", (event) => {
+    if (event.button == 0) {
+        click()
+    }
+}, false);
+
+function click(){
+    player.velocity.x=-(player.position.x-mouseWorld.x)/10
+    player.velocity.y=-(player.position.y-mouseWorld.y)/10
+}
+function spawnEnemy(){
+    enemies[enemies.length]={position:{x:Math.random()*500,y:Math.random()*500}, velocity:{x:0,y:0}}
+}
 function tick(){
     requestAnimationFrame(tick)
     resize()
@@ -34,6 +46,7 @@ function draw(){
     translate()
     ctx.fillRect(mouseWorld.x-5,mouseWorld.y-5,10,10)
     drawPlayer()
+    drawEnemies()
 
 }
 function cameraTick(){
@@ -49,22 +62,38 @@ function playerTick(){
     player.position.x+=player.velocity.x
     player.position.y+=player.velocity.y
     if(kd.D.isDown()){
-        player.velocity.x+=.1
+        player.velocity.x+=1
     }
     if(kd.A.isDown()){
-        player.velocity.x-=.1
+        player.velocity.x-=1
     }
     if(kd.W.isDown()){
-        player.velocity.y-=.1
+        player.velocity.y-=1
     }
     if(kd.S.isDown()){
-        player.velocity.y+=.1
+        player.velocity.y+=1
     }
-    player.velocity.x*=.99
-    player.velocity.y*=.99
+    if(kd.E.isDown()){
+        spawnEnemy()
+    }
+    player.velocity.x*=.95
+    player.velocity.y*=.95
 }
 
+function drawEnemies(){
+    ctx.save()
+    ctx.translate(enemies[0].position.x,enemies[0].position.y)
+    ctx.rotate(Math.atan2(enemies[0].position.y - player.position.y, enemies[0].position.x - player.position.x)-(180*Math.PI/180))
+    ctx.fillStyle = "#33363f"
+    ctx.fillRect(-50,-50,100,100)
+    ctx.fillRect(0,-5,70,10)
+    ctx.fillStyle = "#afbfaf"
+    ctx.fillRect(5-50,5-50,90,90)
+    ctx.restore()
+    
+}
 function drawPlayer(){
+    ctx.save()
     ctx.translate(player.position.x,player.position.y)
     ctx.rotate(Math.atan2(player.position.y - mouseWorld.y, player.position.x - mouseWorld.x)-(180*Math.PI/180))
     console.log(mouseWorld.x, player.position.x)
@@ -73,6 +102,7 @@ function drawPlayer(){
     ctx.fillRect(0,-5,70,10)
     ctx.fillStyle = "#afbfaf"
     ctx.fillRect(5-50,5-50,90,90)
+    ctx.restore()
 
 }
 function resize(){
